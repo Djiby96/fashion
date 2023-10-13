620829c585ccd18e0fac5ea9d48102ff5c02c067
Djiby96 <sarrdjiby20@gmail.com>
2023-10-13 23:42:46 +0200

    V2

--- backend-modules/MySQL.js
+++ backend-modules/MySQL.js
@@ -6,10 +6,10 @@ var mysql = require('mysql');
 
 var con = mysql.createPool({
     connectionLimit: 9000,
-    host: "localhost",
-    user: "Djiby",
-    password: "Djiby_Sarr_1996",
-    database:"product",
+    host: "bbwwsahweyzbgikzshnp-mysql.services.clever-cloud.com",
+    user: "ucqkfpt23is8atb7",
+    password: "IMqQFvT4J7adJe8znpO5",
+    database:"bbwwsahweyzbgikzshnp",
     port: 3306,
     multipleStatements:true
 });
